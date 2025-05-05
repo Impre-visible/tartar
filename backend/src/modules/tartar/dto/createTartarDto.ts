b70a2b81cfe8213a513c1478b0ebc803fd18f7f6
Impre-visible <romeo@chevrier.dev>
2025-05-05 20:06:42 +0200

    ✨ feat: Add CreateTartarDto interface for tartar creation data structure

---
+++ backend/src/modules/tartar/dto/createTartarDto.ts
@@ -0,0 +1,10 @@
+export interface CreateTartarDto {
+    restaurant: string
+    currency: string
+    price: number
+    texture: number
+    taste: number
+    presentation: number
+    totalScore: number
+    createdAt: Date
+}
