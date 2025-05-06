61290eb1d92a900781868fcddad2d1347c6fcac1
Impre-visible <romeo@chevrier.dev>
2025-05-06 20:04:53 +0200

    ✨ feat: Wrap Routes component with ThemeProvider for theme context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 
 import './index.css'
+import { ThemeProvider } from './components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
-  <Routes />
+  <ThemeProvider>
+    <Routes />
+  </ThemeProvider>
 )
