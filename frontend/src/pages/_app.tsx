3c064385e79e49b5bd62fec5dbc7b26400f67ea3
Impre-visible <romeo@chevrier.dev>
2025-05-06 07:26:26 +0200

    ✨ feat: Add padding and adjust header height in RootLayout for improved layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router'
 
 export default function RootLayout() {
     return (
-        <section className="flex flex-col items-center justify-start min-h-screen max-h-screen h-screen min-w-screen max-w-screen w-screen bg-background">
-            <header className="flex items-center justify-center h-20 bg-background">
+        <section className="flex flex-col items-center justify-start min-h-screen max-h-screen h-screen min-w-screen max-w-screen w-screen bg-background p-4 gap-4">
+            <header className="flex items-center justify-center h-fit bg-background">
                 <h1 className="text-3xl font-bold text-foreground">Tartares</h1>
             </header>
             <main className="flex flex-col items-center justify-center h-full w-full">
