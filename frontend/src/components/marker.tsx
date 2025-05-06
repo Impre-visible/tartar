fcf9245487e6973e3dacf9d5a91023527b1eb78c
Impre-visible <romeo@chevrier.dev>
2025-05-06 20:40:19 +0200

    ✨ feat: Add CustomMarker component with HoverCard for enhanced map interactions

---
+++ frontend/src/components/marker.tsx
@@ -0,0 +1,38 @@
+import { MapPin } from 'lucide-react';
+import React from 'react';
+
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+interface MarkerProps {
+    text: string;
+    description?: string;
+    onClick?: () => void;
+    className?: string;
+}
+
+const CustomMarker: React.FC<MarkerProps> = ({ text, description, onClick, className }) => {
+    return (
+
+        <HoverCard>
+            <HoverCardTrigger onClick={onClick}>
+                <div
+                    className={`absolute transform -translate-x-1/2  -translate-y-full cursor-pointer ${className || ''}`}
+                >
+                    <div className="flex flex-col items-center ">
+                        <MapPin className="w-8 h-8 text-white fill-neutral-900 border-neutral-0" />
+                    </div>
+                </div>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-48 p-2 bg-white text-black rounded-md shadow-lg">
+                <div className="text-sm font-semibold">{text}</div>
+                <div className="text-xs text-gray-500">{description ? description : "Click to view details"}</div>
+            </HoverCardContent>
+        </HoverCard>
+    );
+};
+
+export default CustomMarker;
