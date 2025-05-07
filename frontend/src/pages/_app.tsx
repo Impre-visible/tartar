0e6bf5d75278a355cd6e6d7cb159dd6f5e0805f7
Impre-visible <romeo@chevrier.dev>
2025-05-07 22:11:18 +0200

    ✨ feat: Update Toaster component to support rich colors based on theme

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { useTheme } from '@/components/theme-provider';
 import { ModeToggle } from '@/components/ui/mode-toggle';
 import { Toaster } from '@/components/ui/sonner';
 import { Outlet } from 'react-router'
 
 export default function RootLayout() {
+    const { theme } = useTheme();
     return (
         <>
-            <Toaster />
+            <Toaster richColors theme={theme} />
             <section className="flex flex-col items-center justify-start min-h-screen max-h-screen h-screen min-w-screen max-w-screen w-screen bg-background py-4 px-1 md:px-4 gap-4">
                 <header className="w-full flex items-center justify-between h-fit bg-background px-2 md:px-8">
                     <div className="size-9"></div>
